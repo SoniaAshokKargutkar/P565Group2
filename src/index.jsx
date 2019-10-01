97040edf6d4df1838a3a90eeb891cb64333fc8df
Milan Chheta <michheta@iu.edu>
2019-10-01 12:24:15 -0400

    removed src

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Homepage from "./Homepage";
-
-const App = () => <Homepage />;
-
-ReactDOM.render(<App />, document.getElementById("root"));
